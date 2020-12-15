d098551924f2f93151da04b48c8c6ace66f5b301
Aonva <amanda.j.mclaughlin.com>
2020-12-15 20:50:42 +0000

    inishual push

---
+++ pyapi/ride_iss.py
@@ -0,0 +1,30 @@
+#!usr/bin/python3
+"""Alta3 Research -  astros on ISS"""
+
+import urllib.request
+import json
+
+MAJORTOM = "http://api.open-notify.org/astros.json"
+
+def main():
+    """reading json from api"""
+    groundctrl = urllib.request.urlopen(MAJORTOM)
+    helmet = groundctrl.read()
+    print(helmet)
+
+    helmetson = json.loads(helmet.decode("utf-8"))
+
+    print(type(helmet))
+    print(helmetson["number"])
+    print(helmetson["people"])
+    print(helmetson["people"][0])
+    print(helmetson["people"][1])
+    print(helmetson["people"][-1])
+
+    for astro in helmetson["people"]:
+        print(astro)
+    for astro in helmetson["people"]:
+        print(astro["name"])
+
+if __name__ == "__main__":
+    main()
